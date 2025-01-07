e76d2ba0f370c4c7af94ed9af68d45b0ad2780ce
JonathanViera <jonathan_anunciacao@id.uff.br>
2025-01-07 19:50:07 -0300

    Update trabalho.c
    Atualização do codigo

--- trabalho.c
+++ trabalho.c
@@ -1,20 +1,26 @@
 #include "bib.h"
 
-int main() {
-    int* a = (int*)malloc2(sizeof(int));
-    int* b = (int*)malloc2(sizeof(int));
+int main()
+{
+    int *a = (int *)malloc2(sizeof(int));
+    int *b = (int *)malloc2(sizeof(int));
 
     *a = 10;
     *b = 20;
+   
+    printf("Antes: a = %d, b = %d \n", *a, *b);
+    dump();
 
-    printf("Antes: a = %d, b = %d\n", *a, *b);
-
-    atrib2((void**)&b, a); // b agora aponta para o mesmo bloco que a
-
+    atrib2((void **)&b, a);// b agora aponta para o mesmo bloco que a
+    atrib2((void**)&c, a);
     printf("Depois: a = %d, b = %d\n", *a, *b);
+    dump();
 
-    atrib2((void**)&a, NULL); // Libera o bloco de a
-    atrib2((void**)&b, NULL); // Libera o bloco de b
+    atrib2((void **)&a, NULL); // Libera o bloco de a
+    atrib2((void **)&b, NULL);
+     // Libera o bloco de b
+
+    dump();
 
     return 0;
 }
